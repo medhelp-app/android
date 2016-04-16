910e236bb0580ded1e65b3e253070c4f927a2cd5
CarlosRodrigo <carlosrodrigocrcg@gmail.com>
2016-04-16 20:08:25 -0300

    Add rest api call to signup service

--- app/src/main/java/com/medhelp/medhelp/SignUpActivity.java
+++ app/src/main/java/com/medhelp/medhelp/SignUpActivity.java
@@ -2,12 +2,155 @@ package com.medhelp.medhelp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.medhelp.medhelp.exceptions.PasswordInvalidException;
+import com.medhelp.medhelp.helpers.authenticationValidator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class SignUpActivity extends Activity {
+    private static final String TAG = "SignupActivity";
+
+    // TODO: Change url to production
+    private static final String SIGNUP_URL = "http://192.168.1.X:4000/api/users";
+
+    @Bind(R.id.input_name_signup) EditText _nameText;
+    @Bind(R.id.input_email_signup) EditText _emailText;
+    @Bind(R.id.input_password_signup) EditText _passwordText;
+    @Bind(R.id.input_confirmationPassword_signup) EditText _passwordConfirmationText;
+    @Bind(R.id.btn_signup_signup) Button _signupButton;
+
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+
+        _signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+
+    }
+
+    private void signup() {
+        Log.d(TAG, "Signup");
+
+        _signupButton.setEnabled(false);
+
+        String name = _nameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+        String passwordConfirmation = _passwordConfirmationText.getText().toString();
+
+        if (validateName(name) && validateEmail(email) && validatePassword(password) && validatePasswordConfirmation(password, passwordConfirmation))
+            register(name, email, password, passwordConfirmation);
+        else
+            _signupButton.setEnabled(true);
+    }
+
+    private void register(final String name, final String email, final String password, final String passwordConfirmation) {
+        StringRequest request = new StringRequest(Request.Method.POST, SIGNUP_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Cadastro realizado com sucesso");
+                Toast.makeText(getBaseContext(), "Cadastro realizado com sucesso", Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "Cadastro falhou");
+
+                if (!TextUtils.isEmpty(error.getMessage()))
+                    onLoginFailed(error.getMessage());
+                else
+                    onLoginFailed("Cadastro falhou");
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("name", name);
+                params.put("email", email);
+                params.put("password", password);
+                params.put("rePassword", passwordConfirmation);
+
+                return params;
+            }
+        };
+
+        requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
+
+        _signupButton.setEnabled(true);
+    }
+
+    private boolean validateName(String name) {
+        if (TextUtils.isEmpty(name)) {
+            _nameText.setError("Campo obrigatório");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateEmail(String email) {
+        if (!authenticationValidator.isValidEmail(email)) {
+            _emailText.setError("Entre um email válido");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validatePassword(String password) {
+        try {
+            authenticationValidator.isValidPassword(password);
+        } catch (PasswordInvalidException ex) {
+            _passwordText.setError(ex.getMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean validatePasswordConfirmation(String password, String passwordConfirmation) {
+        try {
+            authenticationValidator.isValidPassword(passwordConfirmation);
+        } catch (PasswordInvalidException ex) {
+            _passwordConfirmationText.setError(ex.getMessage());
+            return false;
+        }
+
+        if (!passwordConfirmation.equals(password)) {
+            _passwordConfirmationText.setError("Senhas devem ser iguais");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void onLoginFailed(String message) {
+        Toast.makeText(getBaseContext(), message, Toast.LENGTH_LONG).show();
+        _signupButton.setEnabled(true);
     }
 }
