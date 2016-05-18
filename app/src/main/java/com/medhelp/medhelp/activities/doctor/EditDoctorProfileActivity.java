93abfee3370614475c7c7132dea4f34ab3592634
CarlosRodrigo <carlosrodrigocrcg@gmail.com>
2016-05-17 22:35:05 -0300

    Finish doctor profile tab

---
+++ app/src/main/java/com/medhelp/medhelp/activities/doctor/EditDoctorProfileActivity.java
@@ -0,0 +1,16 @@
+package com.medhelp.medhelp.activities.doctor;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.medhelp.medhelp.R;
+
+public class EditDoctorProfileActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_doctor_profile);
+    }
+
+}
--- app/src/main/java/com/medhelp/medhelp/fragments/doctor/DoctorProfileFragment.java
+++ app/src/main/java/com/medhelp/medhelp/fragments/doctor/DoctorProfileFragment.java
@@ -1,21 +1,20 @@
 package com.medhelp.medhelp.fragments.doctor;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.medhelp.medhelp.R;
+import com.medhelp.medhelp.activities.doctor.EditDoctorProfileActivity;
 
 public class DoctorProfileFragment extends Fragment {
 
     public DoctorProfileFragment() {
         // Required empty public constructor
-    }
-
-    public static DoctorProfileFragment newInstance(String param1, String param2) {
-        return new DoctorProfileFragment();
     }
 
     @Override
@@ -27,7 +26,19 @@ public class DoctorProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_doctor_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_doctor_profile, container, false);
+
+        FloatingActionButton fabEditPatient = (FloatingActionButton) view.findViewById(R.id.fab_edit_doctor_doctorProfile);
+        fabEditPatient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), EditDoctorProfileActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 
 }
