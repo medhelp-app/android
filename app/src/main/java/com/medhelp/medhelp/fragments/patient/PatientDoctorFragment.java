3d56348347b68d7f5761b924f141c416eb70acb8
CarlosRodrigo <carlosrodrigocrcg@gmail.com>
2016-05-25 23:48:23 -0300

    Add missing layout

--- app/src/main/java/com/medhelp/medhelp/fragments/patient/PatientDoctorFragment.java
+++ app/src/main/java/com/medhelp/medhelp/fragments/patient/PatientDoctorFragment.java
@@ -17,7 +17,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
