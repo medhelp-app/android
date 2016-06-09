61a8c2726407f51e781baffa807d9ac496869f00
CarlosRodrigo <carlosrodrigocrcg@gmail.com>
2016-06-09 20:44:17 -0300

    Tab icons

--- app/src/main/java/com/medhelp/medhelp/activities/doctor/MainDoctorActivity.java
+++ app/src/main/java/com/medhelp/medhelp/activities/doctor/MainDoctorActivity.java
@@ -10,16 +10,27 @@ import com.medhelp.medhelp.views.adapters.DoctorPagerAdapter;
 
 public class MainDoctorActivity extends AppCompatActivity {
 
+    private TabLayout mTabLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_doctor);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.doctor_sections_tab);
+        mTabLayout = (TabLayout) findViewById(R.id.doctor_sections_tab);
         ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager_doctorMainTab);
 
         viewPager.setAdapter(new DoctorPagerAdapter(getSupportFragmentManager(), getResources().getStringArray(R.array.titles_doctorTab)));
-        tabLayout.setupWithViewPager(viewPager);
+        mTabLayout.setupWithViewPager(viewPager);
+
+        setupTabIcons();
+    }
+
+    private void setupTabIcons() {
+        mTabLayout.getTabAt(0).setIcon(R.drawable.ic_accessibility_white_24dp);
+        mTabLayout.getTabAt(1).setIcon(R.drawable.ic_account_circle_white_24dp);
+        mTabLayout.getTabAt(2).setIcon(R.drawable.ic_assignment_ind_white_24dp);
+        mTabLayout.getTabAt(3).setIcon(R.drawable.ic_chat_white_24dp);
     }
 
 }
--- app/src/main/java/com/medhelp/medhelp/activities/patient/MainActivity.java
+++ app/src/main/java/com/medhelp/medhelp/activities/patient/MainActivity.java
@@ -10,16 +10,27 @@ import com.medhelp.medhelp.views.adapters.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TabLayout mTabLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.sections_tab);
+        mTabLayout = (TabLayout) findViewById(R.id.sections_tab);
         ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager_mainTab);
 
         viewPager.setAdapter(new SectionsPagerAdapter(getSupportFragmentManager(), getResources().getStringArray(R.array.titles_patientTab)));
-        tabLayout.setupWithViewPager(viewPager);
+        mTabLayout.setupWithViewPager(viewPager);
+
+        setupTabIcons();
+    }
+
+    private void setupTabIcons() {
+        mTabLayout.getTabAt(0).setIcon(R.drawable.ic_accessibility_white_24dp);
+        mTabLayout.getTabAt(1).setIcon(R.drawable.ic_account_circle_white_24dp);
+        mTabLayout.getTabAt(2).setIcon(R.drawable.ic_folder_white_24dp);
+        mTabLayout.getTabAt(3).setIcon(R.drawable.ic_chat_white_24dp);
     }
 
 }
